e3842f6b41eea8138bd18825ae8ffd46c6498d99
Ravikumarchavva <chavvaravikumarreddy2004@gmail.com>
2024-08-19 23:14:08 +0530

    changed to post methods

--- docker/main.py
+++ docker/main.py
@@ -1,66 +1,47 @@
-from typing import Union
-
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import random
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from the .env file
+# Load environment variables
 load_dotenv()
 
-# Access the environment variable
+# Access environment variables
 BASE_URL = os.getenv("BASE_URL")
 NEXTJS_URL = os.getenv("NEXTJS_URL")
+
 app = FastAPI()
 
+# CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[BASE_URL,NEXTJS_URL],
+    allow_origins=[BASE_URL, NEXTJS_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
+class ChurnPredictionRequest(BaseModel):
+    tenure: str
+    contract: str
+    monthlyCharges: str
+    internetService: str
+    paymentMethod: str
 
 @app.get("/")
 def read_root():
-    return {"Hello next World"}
+    return {"message": "Hello, Next.js World!"}
 
-items = {
-    1: {"name": "Foo", "price": 42.00},
-    2: {"name": "Bar", "price": 24.00, "is_offer": True},
-    3: {"name": "Baz", "price": 19.50},
-}
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id, "details": items.get(item_id, {})}
+@app.post("/send_carprice")
+def carprice():
+    return {"price": random.randint(1000, 50000)}
 
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-import apis.carprice as cp
-@app.get('/carprice')
-def get_car_price():
-    return {"price": cp.carprice()}
-
-import apis.churn as ch
-
-@app.get('/churn')
-def get_churn_prediction():
-    return {"churn":ch.churn}
-
-import apis.imageClassification as c
-
-@app.post('/imageclassification')
-def classify_image():
-    return {"image":c.imageClassification()}
+@app.post("/predict_churn")
+def predict_churn(request: ChurnPredictionRequest):
+    try:
+        result = random.choice(["Churn", "Don't Churn"])
+        return {"prediction": result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
